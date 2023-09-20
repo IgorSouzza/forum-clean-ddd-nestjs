8d15775a6d0252a451215900a7379c039ad09264
Igor Souza <igor.souza.webmaster@gmail.com>
2023-09-20 11:00:18 -0300

    feat: add auth guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
+
 import { Env } from 'src/env'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
+  providers: [JwtStrategy],
   imports: [
     PassportModule,
     JwtModule.registerAsync({
